98e356469d61aaaa0a3e9458621438a362e24f35
arash-gmc <arash.green.423@gmail.com>
2023-03-03 21:23:13 +0330

    arash

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-	res.send('helloo')
+	res.send('arash')
 })
 
 app.listen(3000,()=>{console.log('listening on port 3000...')})
